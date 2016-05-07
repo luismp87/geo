9df6b8f4f0f02838af0a5f3a5890e2c0c5e3f9bb
luismp87 <luismp87@gmail.com>
2016-05-07 10:22:13 -0500

    ss

---
+++ actions.js
@@ -0,0 +1,30 @@
+			var fn = {
+                ready: function(){
+                    
+                  document.addEventListener(deviceready,fn.init,false);  
+                },
+                init: function(){
+                //fn.dibujarMapa(19.060679, -98.201306);
+                    navigator.geolocation.getCurrentPosition(fn.posicionObtenida,fn.error);
+                    },
+                    posicionObtenida: function(p){
+                  fn.dibujarMapa(p.coords.latitude,p.coords.latitude);      
+                    },
+                    dibujarMapa: function(lat,lng){
+				//Posición del mapa
+				var latlng = new google.maps.LatLng(lat,lng);
+				var myOptions = {
+					zoom: 8,
+					center: latlng,
+					mapTypeId: google.maps.MapTypeId.ROADMAP
+				};
+				var map = new google.maps.Map(document.getElementById("map_canvas"),myOptions);
+				//Marcador
+				var marker = new google.maps.Marker({
+					position: latlng, 
+					map: map, 
+					title:"Mi posición"
+				});
+			},
+};
+$(fn.init);
